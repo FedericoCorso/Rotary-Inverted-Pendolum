b29506b7210f44b8912e92e99c7e32108ffebb6c
Federico Corso <fede1799@gmail.com>
2023-03-04 00:53:15 +0100

    SS representation

--- src/matlab scrripts/SS.m
+++ src/matlab scrripts/SS.m
@@ -7,7 +7,7 @@ B_p = 0.0024; % Pendulum viscous damping coeffi- cient as seen at the pivot axis
 B_r = 0.0024; % Rotary arm viscous damping coeffi- cient as seen at the pivot axis N*m*s/rad
 J_a = 0.0020; % Rotary arm moment of inertia about pivot m^2
 J_p = 0.0012; % Pendulum moment of intertia about center of mass Kg*m^2
-l_p = (0.337)/2;%0.156; % Distance from pivot to center of mass m
+l_p = 0.156; % Distance from pivot to center of mass m
 m_p = 0.127; % Mass of pendulum Kg
 r = 0.216; % Rotary arm length from pivot to tip m
 J_T = 2*(J_a*m_p*l_p^2 + J_p*m_p*r^2 + J_a*J_p);
@@ -22,7 +22,7 @@ k_m = 7.68e-3; % Motor back-emf constant V/(rad/s)
 k_t = 7.68e-3; % Motor current-torque constant N*m/A
 
 %% build the state space matrices
-[A,B,C,D] = SS_Matrices(B_p,B_r,J_T,J_a,J_p,K_g,R_m,eta_g,eta_m,g,k_m,k_t,l_p,m_p,r);
+[A,B,C,D] = SS_Matrices(B_p,B_r,J_a,J_p,K_g,R_m,eta_g,eta_m,g,k_m,k_t,l_p,m_p,r);
 
 % State-space model
 system = ss(A,B,C,D);
