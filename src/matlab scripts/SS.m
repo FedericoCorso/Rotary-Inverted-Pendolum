f5014abe9015cd4025941fdd0eadffb83c00d001
Federico Corso <fede1799@gmail.com>
2023-03-14 15:19:33 +0100

    To Do: change sign of voltage signal

---
+++ src/matlab scripts/SS.m
@@ -0,0 +1,45 @@
+startUp
+
+close all
+
+%% build the state space matrices
+[A,B,C,D] = SS_Matrices_Voltage_Stable(B_p,B_r,J_T,J_a,J_p,K_g,R_m,eta_g,eta_m,g,k_m,k_t,l_p,m_p,r);
+% [A,B,C,D] = SS_Matrices_Voltage_Unstable(B_p,B_r,J_T,J_a,J_p,K_g,R_m,eta_g,eta_m,g,k_m,k_t,l_p,m_p,r);
+% [A,B,C,D] = SS_Matrices_Torque_Stable(B_p,B_r,J_T,J_a,J_p,g,l_p,m_p,r);
+% [A,B,C,D] = SS_Matrices_Torque_Unstable(B_p,B_r,J_T,J_a,J_p,g,l_p,m_p,r);
+C = eye(4);
+D = zeros(4,1);
+% State-space model
+system = ss(A,B,C,D);
+
+system.A
+system.B
+system.C
+system.D
+
+% eigenvalues of the system
+eigenvalues = eig(A);
+
+% bandwidth(system)
+w = 0:0.001:1000;
+[mag,phase,wout] = bode(system,w);
+
+% conversion to logarithm scale
+mag_theta = 20*log10(mag(1,:));
+mag_alpha = 20*log10(mag(2,:));
+mag_theta_dot = 20*log10(mag(3,:));
+mag_alpha_dot = 20*log10(mag(4,:));
+
+% separated bode plots
+figure
+semilogx(w,mag_theta),grid on
+figure
+semilogx(w,mag_alpha),grid on
+figure
+semilogx(w,mag_theta_dot),grid on
+figure
+semilogx(w,mag_alpha_dot),grid on
+
+bandwidth_theta_dot = wout(4180,1);
+
+
