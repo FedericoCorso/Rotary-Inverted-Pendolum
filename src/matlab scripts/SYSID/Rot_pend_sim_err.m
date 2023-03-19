fb4e0aef7ccf0034f8cf3abd03b52aa34de9ce2f
Federico Corso <fede1799@gmail.com>
2023-03-19 12:25:52 +0100

    added metrics to asses goodness of estimated results and residual analysis

--- src/matlab scripts/SYSID/Rot_pend_sim_err.m
+++ src/matlab scripts/SYSID/Rot_pend_sim_err.m
@@ -30,13 +30,41 @@ Nsim_FFD = size(ymeas,2); % number of samples
 zhat_FFD = zeros(4,Nsim_FFD); % matrix to contain the trajectories
 zhat_FFD(:,1) = z0; % initial state 
 
+%Lin
+zhat_FFD_corrected = zeros(4,Nsim_FFD);
+zhat_FFD_corrected(:,1) = [ymeas(1,1);ymeas(2,1);0;0];
+
+%% simulation of the non linear model
 for ind=2:Nsim_FFD
     zdot = NL_eqns(0,zhat_FFD(:,ind-1),uin(:,ind-1),th);
     zhat_FFD(:,ind)    =   zhat_FFD(:,ind-1)+Ts*zdot;
 
 end
 
-%% Run simulation with ode45
+%% Simulation of the linear model
+% for ind=2:Nsim_FFD
+%     zdot = Lin_eqns(0,zhat_FFD(:,ind-1),uin(:,ind-1),th);
+%     zhat_FFD(:,ind)    =   zhat_FFD(:,ind-1)+Ts*zdot;
+%     zhat_FFD_corrected(:,ind)    = zhat_FFD(:,ind) + [ymeas(1,1);ymeas(2,1);0;0];
+% end
+
+%% Collect simulated output (forward speed and yawrate)
+% NL system
+ysim = zhat_FFD;
+
+% Lin sys
+%ysim = zhat_FFD_corrected;
+
+
+
+%% compute weighted error
+err = Q*(ymeas-ysim);
+
+%% stack errors in one vector - use only position error
+err_vec = [err(1,:) err(2,:)]'/sqrt(Nsim_FFD); % err(3,:) err(4,:)
+
+
+%% Run simulation with ode45 - too expensive - leave commented
 % Ts_VSS = 0.002; % 2ms
 % Nsim_VSS = size(ymeas,2);% number of measured samples
 % zhat_VSS = zeros(4,Nsim_VSS); % matrix to contain the trajectories
@@ -46,16 +74,7 @@ end
 %     temp = ode45(@(t,z)NL_eqns(t,z,uin(:,ind),th),[0 Ts_VSS],zhat_VSS(:,ind));
 %     zhat_VSS(:,ind+1) = temp.y(:,end);
 % end
-
-%% Collect simulated output (forward speed and yawrate)
-% ysim = [zhat_FFD(1,:); zhat_FFD(2,:)]; % the measured output is equal to the state
-
-ysim = zhat_FFD;
 % ysim = zhat_VSS(:,end-1);
-%% compute weighted error
-err = Q*(ymeas-ysim);
-
-%% stack errors in one vector
-err_vec = [err(1,:) err(2,:) err(3,:) err(4,:)]'/sqrt(Nsim_FFD); % err(3,:) err(4,:)
 % err_vec = [err(1,:) err(2,:)]'/sqrt(Nsim_VSS); % err(3,:) err(4,:)
+
 end
